ae962c099c033e4d46830126bc8f527232cf577c
Alireza Asadi <alireza.asadi.36@gmail.com>
2024-02-07 12:27:56 +0330

    test: Add tests for @BuildableIgnored.

---
+++ Tests/BuildableMacroTests/BuildableIngoredMacroTests.swift
@@ -0,0 +1,37 @@
+import SwiftSyntaxMacros
+import SwiftSyntaxMacrosTestSupport
+import XCTest
+import MacroTesting
+
+import BuildableMacros
+
+final class BuildableIngoredMacroTests: XCTestCase {
+    override func invokeTest() {
+        withMacroTesting(
+            isRecording: false,
+            macros: ["BuildableIgnored": BuildableIgnoredMacro.self]
+        ) {
+            super.invokeTest()
+        }
+    }
+
+    func testNoChangeForBuildableIgnoredProperties() throws {
+        assertMacro {
+            """
+            struct Sample {
+                @BuildableIgnored
+                var p1: String
+                @BuildableIgnored
+                var p2: String = ""
+            }
+            """
+        } expansion: {
+            """
+            struct Sample {
+                var p1: String
+                var p2: String = ""
+            }
+            """
+        }
+    }
+}
