0e85d815c9c0cb3625cafe12f80e4b7add2ca988
Seyed Mojtaba Hosseini Zeidabadi <Chenzook+GitHub@gmail.com>
2024-02-16 22:21:53 +0330

    chore: remove unnecessary characters

--- Sources/BuildableMacros/Foundation+Extensions.swift
+++ Sources/BuildableMacros/Foundation+Extensions.swift
@@ -1,5 +1,5 @@
 extension Collection where Element: Equatable {
     func contains<C: Collection>(anyOf collection: C) -> Bool where C.Element == Element {
-        contains(where: collection.contains(_:))
+        contains(where: collection.contains)
     }
 }
